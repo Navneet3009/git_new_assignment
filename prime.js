417de41fde92f6ad856ce5c84fbdad6a18901478
Navneet3009 <navneetsrivastava52@gmail.com>
2022-11-04 00:05:41 +0530

    problem_resolved

--- prime.js
+++ prime.js
@@ -3,8 +3,8 @@ let number=13;
 let count=0;
 for(let i=1; i<=number;i++){
 if(number%i==0){
+count++}
 }
-}
-if(count!=2){
+if(count==2){
 console.log(number,"is a Prime Number");}
 else {console.log(number, "is not a Prime Number");}
