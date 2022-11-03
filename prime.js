af05565de8e50dbd6c5911484876f7cf8537edf0
Navneet3009 <navneetsrivastava52@gmail.com>
2022-11-04 00:01:36 +0530

    Prime Problem

---
+++ prime.js
@@ -0,0 +1,10 @@
+//prime problem
+let number=13;
+let count=0;
+for(let i=1; i<=number;i++){
+if(number%i==0){
+}
+}
+if(count!=2){
+console.log(number,"is a Prime Number");}
+else {console.log(number, "is not a Prime Number");}
